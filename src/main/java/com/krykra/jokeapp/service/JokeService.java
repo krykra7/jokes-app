6f19911f82afca012fc04bcc602a9d108324539f
Krystian Krawczyk <krykra7@gmail.com>
2018-08-27 12:08:53 +0200

    Created service for jokes

--- src/main/java/com/krykra/jokeapp/service/JokeService.java
+++ src/main/java/com/krykra/jokeapp/service/JokeService.java
@@ -4,4 +4,6 @@ package com.krykra.jokeapp.service;
  * Created by Krystian Krawczyk on 8/27/2018.
  */
 public interface JokeService {
+
+    String getJoke();
 }
--- src/main/java/com/krykra/jokeapp/service/JokeServiceImpl.java
+++ src/main/java/com/krykra/jokeapp/service/JokeServiceImpl.java
@@ -1,7 +1,24 @@
 package com.krykra.jokeapp.service;
 
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
 /**
  * Created by Krystian Krawczyk on 8/27/2018.
  */
-public class JokeServiceImpl {
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
 }
